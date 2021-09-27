d0f6215c03ed99e78d6e5b3ad7bcd8f6b0a4c108
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-26 23:46:12 -0300

    Adiciona atividade JurosSimples

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objetivo: Calcule o montante final de um investimento a juros simples.
+
+            Console.WriteLine("         Juros Simples (j)         \n");
+
+            Console.WriteLine("Insira as informações solicitadas a seguir:\n");
+
+            Console.Write("Capital [c] (R$).......: ");
+            float c = float.Parse(Console.ReadLine());
+            Console.Write("Taxa de juros [i] (%)..: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.Write("Tempo [t] (meses)......: ");
+            int t = int.Parse(Console.ReadLine());
+
+            float j = ((c*i)/100)*t;
+
+            Console.WriteLine($"\nJuros (R$) ............: {j}");
+            Console.WriteLine($"Montante (R$)......: {c+j}");
+        }
+    }
+}
